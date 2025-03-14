06b78630c9b4bd508a0128e227d5825ad858301c
Bella <slin56891@gmail.com>
2025-03-14 19:15:58 +0800

    fix version conflict and create redux store

---
+++ my-app/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
+
+export default store;
